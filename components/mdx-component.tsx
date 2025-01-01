dfdd668ffc2dc93a6ded16bbce84a0b4f602479b
complexdev <carminech@proton.me>
2025-01-01 16:32:11 +0100

    Add Callout component and integrate it into the blog post

--- components/mdx-component.tsx
+++ components/mdx-component.tsx
@@ -1,5 +1,6 @@
 import Image from 'next/image';
 import * as runtime from 'react/jsx-runtime';
+import { Callout } from './callout';
 
 const useMDXComponents = (code: string) => {
     const fn = new Function(code);
@@ -7,7 +8,8 @@ const useMDXComponents = (code: string) => {
 };
 
 const components = {
-    Image
+    Image,
+    Callout
 }
 
 interface mdxProps {
